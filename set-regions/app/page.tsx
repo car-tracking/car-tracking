55181aaabb54114aaa6482946122ffc4f1e9515b
qrtz <81661096+qt2@users.noreply.github.com>
2023-12-08 11:46:31 +0900

    svgで線描画の実装

--- set-regions/app/page.tsx
+++ set-regions/app/page.tsx
@@ -6,9 +6,15 @@ import { IoTrashOutline } from "react-icons/io5";
 
 export default function Home() {
   const [points, setPoints] = useState([[0.3, 0.5]]);
+  const [lines, setLines] = useState([
+    [
+      [0.1, 0.1],
+      [0.2, 0.2],
+    ],
+  ]);
   const [num, setNum] = useState(3);
 
-  const canvasRef = useRef<HTMLDivElement>(null);
+  const canvasRef = useRef<SVGSVGElement>(null);
   const [canvasSize, setCanvasSize] = useState<{ w: number; h: number }>({
     w: 100,
     h: 100,
@@ -24,12 +30,14 @@ export default function Home() {
   useEffect(() => {
     updateCanvasSize();
 
-    window.addEventListener("resize", () => {
-      updateCanvasSize();
-    });
+    window.addEventListener("resize", updateCanvasSize);
+
+    () => {
+      window.removeEventListener("resize", updateCanvasSize);
+    };
   }, []);
 
-  const createItem = (e: React.MouseEvent<HTMLDivElement>) => {
+  const createItem = (e: React.MouseEvent) => {
     if (e.target === e.currentTarget) {
       const point = [e.clientX / canvasSize.w, e.clientY / canvasSize.h];
       setPoints([...points, point]);
@@ -40,23 +48,82 @@ export default function Home() {
     <main className="max-w-screen-lg mx-auto">
       <div className="relative w-full">
         <img src="/sample.png" alt="" className="w-full" />
-        <div
+        <svg
           ref={canvasRef}
           className="absolute top-0 left-0 right-0 bottom-0 "
+          width="100%"
+          height="100%"
+          preserveAspectRatio="false"
           onMouseDown={createItem}
         >
+          <defs>
+            <marker
+              id="arrow"
+              viewBox="0 0 10 10"
+              refX="5"
+              refY="5"
+              markerWidth="6"
+              markerHeight="6"
+              orient="auto-start-reverse"
+            >
+              <path d="M 0 0 L 10 5 L 0 10 z" />
+            </marker>
+          </defs>
           {points.map(([x, y]) => (
-            <div
-              onClick={() => {}}
-              onDrag={() => {}}
-              className="absolute w-2 h-2 rounded-full bg-white ring-2 ring-primary -translate-x-1/2 -translate-y-1/2"
-              style={{
-                left: x * canvasSize.w,
-                top: y * canvasSize.h,
+            <circle
+              onClick={() => {
+                console.log("click");
               }}
-            ></div>
+              onDrag={() => {}}
+              cx={`${x * 100}%`}
+              cy={`${y * 100}%`}
+              r={4}
+              fill="white"
+              strokeWidth={2}
+              stroke="#f0f"
+            ></circle>
           ))}
-        </div>
+
+          {lines.map((vs, i) => {
+            const [[x1, y1], [x2, y2]] = vs;
+            const [cx, cy] = [(x1 + x2) / 2, (y1 + y2) / 2];
+            const [x1p, y1p, x2p, y2p, cxp, cyp] = [x1, y1, x2, y2, cx, cy].map(
+              (v) => `${v * 100}%`
+            );
+            return (
+              <>
+                <line
+                  x1={x1p}
+                  y1={y1p}
+                  x2={x2p}
+                  y2={y2p}
+                  strokeWidth={2}
+                  stroke="aqua"
+                  markerStart="url(#arrow)"
+                />
+                <text x={cxp} y={cyp} fill="aqua">
+                  {i}
+                </text>
+                <circle
+                  cx={x1p}
+                  cy={y1p}
+                  r={4}
+                  fill="white"
+                  strokeWidth={2}
+                  stroke="aqua"
+                />
+                <circle
+                  cx={x2p}
+                  cy={y2p}
+                  r={4}
+                  fill="white"
+                  strokeWidth={2}
+                  stroke="aqua"
+                />
+              </>
+            );
+          })}
+        </svg>
       </div>
 
       <div className="overflow-auto">
