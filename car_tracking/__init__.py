22b43e84020a76b3f3d4a7c2638a43159920f21b
Mae Yamaguchi <yamaguchi-mae512@g.ecc.u-tokyo.ac.jp>
2023-12-21 13:49:46 +0900

    fix

--- car_tracking/__init__.py
+++ car_tracking/__init__.py
@@ -3,6 +3,7 @@ from ultralytics import YOLO
 import supervision as sv
 from tqdm import tqdm
 import cv2 as cv
+import numpy as np
 
 
 def track(
@@ -53,7 +54,7 @@ def track(
 
     # 車両の流入・流出を記録するためのdictionary
     flow_ins = dict()
-    counts = [[0 for _ in range(len(lines))] for _ in range(len(lines))]
+    counts = np.zeros((len(lines), len(lines)), dtype=np.int16)
 
     # 動画の各フレームに対して処理を実行
     for frame in tqdm(video, total=video_info.total_frames):
@@ -68,12 +69,12 @@ def track(
         for i, line_zone in enumerate(line_zones):
             (flow_in, flow_out) = line_zone.trigger(detections)
 
-            for j, detect in enumerate(flow_in):
-                if detect == True:
-                    flow_ins[detections.tracker_id[j]] = i
-            for j, detect in enumerate(flow_out):
-                if detect == True and detections.tracker_id[j] in flow_ins:
-                    from_line = flow_ins[detections.tracker_id[j]]
+            for detect, tracker_id in zip(flow_in, detections.tracker_id):
+                if detect:
+                    flow_ins[tracker_id] = i
+            for detect, tracker_id in zip(flow_out, detections.tracker_id):
+                if detect and tracker_id in flow_ins:
+                    from_line = flow_ins[tracker_id]
                     to_line = i
                     counts[to_line][from_line] += 1
 
