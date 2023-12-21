3e34e67f5bcfe4cd93a3e5986c72813e07ecc725
sugamaron <sugamaron10@gmail.com>
2023-12-21 15:44:32 +0900

    bench.json取得

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import fire
 from pathlib import Path
 import json
 import car_tracking
+import sys
 
 
 def cli(
@@ -9,6 +10,7 @@ def cli(
     regions: str = None,
     output: str = None,
     no_show: bool = False,
+    bench: str = None,
 ):
     if regions is None:
         regions = Path(source).with_suffix(".json")
@@ -17,6 +19,21 @@ def cli(
         lines: list[list[float]] = json.load(f)
 
     car_tracking.track(source, lines, output, no_show)
+
+    with open(bench) as f:
+        bench_file = json.load(f)
+        kind = bench_file["kind"]
+        data = bench_file["data"]
+
+        if kind == "line":
+            pass
+        elif kind == "from_to":
+            pass
+        elif kind == "type":
+            pass
+        else:
+            print("error: kind is invalid.", file=sys.stderr)
+            return
 
 
 if __name__ == "__main__":
